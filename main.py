838b4544d8181efe482105d37ed5c393ac5c12bf
aidenvandenbush <aiden.vandenbush@gmail.com>
2024-11-05 11:23:46 -0600

    new dataset

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ from langchain_community.vectorstores import FAISS
 from langchain.chains import RetrievalQA
 from langchain_openai import OpenAI
 from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 if not os.getenv("OPENAI_API_KEY"):
-    os.environ["OPENAI_API_KEY"] = ""
+    os.environ["OPENAI_API_KEY"] = "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"
 
 # Load the dataset
-df = pd.read_csv('C:/Users/aiden/Source/Repos/AutoQuote/parent_reply.csv')
+df = pd.read_csv('C:/Users/aiden/Source/Repos/AutoQuote/quotes.csv')
 
+texts = df['quote'].to_list()
+authors = df['author'].to_list()
+newTexts = []
+indices = []
+newAuthors = []
+for i in range(len(texts)):
+    indices.append({'id': str(i)})
+    newTexts.append(str(texts[i]))
+    newAuthors.append(str(authors[i]))
 
 index_path = "faiss_index"
 
@@ -25,7 +36,7 @@ if os.path.exists(index_path):
 else:
     # Create a FAISS vector store
     print("Creating Vector Store")
-    vector_store = FAISS.from_texts(df['reply'].tolist(), OpenAIEmbeddings())
+    vector_store = FAISS.from_texts(newTexts[:1000], OpenAIEmbeddings(), metadatas=indices[:1000])
     # Save the new FAISS index locally
     vector_store.save_local(index_path)
     print("Created and saved new FAISS index")
@@ -35,12 +46,12 @@ llm = OpenAI()  # Set any necessary parameters here
 
 @app.route('/data/<userInput>')
 def get_quote(userInput):
-    quotes = vector_store.similarity_search(userInput)
+    quotes = vector_store.similarity_search(userInput, k=5)
 
     prompt = f"Out of all of the quotes below, return the one most similar to the quote '{userInput}': "
-    
     for quote in quotes:
-        prompt += "\n - " + quote.page_content
+        print(quote.metadata)
+        prompt += "\n - " + quote.page_content + ' - ' + authors[int(quote.metadata['id'])]
     
     print(prompt)
 
